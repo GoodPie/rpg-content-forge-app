f9889062448c2c7fdf5d1ed54e545b16eea066eb
Brandyn Britton <Brandynbb96@gmail.com>
2025-04-23 17:13:00 +0800

    chore: add Jest configuration for testing

---
+++ jest.setup.js
@@ -0,0 +1,15 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// Mock Next.js router
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+    back: jest.fn(),
+    forward: jest.fn(),
+  }),
+  usePathname: () => '',
+  useSearchParams: () => new URLSearchParams(),
+}));
